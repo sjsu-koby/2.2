6f960f943bd74d1229332b976017d53ebd7066d1
sjsu-koby <koby.nguyen@sjsu.edu>
2022-03-24 03:50:50 -0700

    i did a lot of stuff

--- sketch.js
+++ sketch.js
@@ -26,13 +26,9 @@ var randomIndex;
 var counter = 0;
 var introdogs;
 var parkBackground;
-var dog0;
-var dog1;
-var dog2;
-var dog3;
-var dog4;
 var dogs = [];
 var button;
+var artButton;
 
 function preload() {
 
@@ -44,11 +40,7 @@ function preload() {
 
 
   }
-  // dog0 = loadImage(`images/dog0.jpg`);
-  // dog1 = loadImage(`images/dog1.jpg`);
-  // dog2 = loadImage(`images/dog2.jpg`);
-  // dog3 = loadImage(`images/dog3.jpg`);
-  // dog4 = loadImage(`images/dog4.jpg`);
+
 }
 
 function setup() {
@@ -61,6 +53,9 @@ function setup() {
   button.mousePressed(clickButton);
   button.class("randomButton");
   button.position(width * .39, height * 1);
+  artButton = select("#artistButton");
+  artButton.class("artistButton");
+  artButton.position(width * .44, height * 1.2);
 }
 
 function draw() {
@@ -70,32 +65,6 @@ function draw() {
 function drawDogIntro() {
 
   image(introdogs, width * .1, height * .2);
-
-}
-
-
-function drawDog0() {
-  image(dog0, width * .1, height * .1)
-
-}
-
-function drawDog1() {
-  image(dog1, width * .1, height * .1)
-
-}
-
-function drawDog2() {
-  image(dog2, width * .13, height * .1)
-
-}
-
-function drawDog3() {
-  image(dog3, width * .1, height * .15)
-
-}
-
-function drawDog4() {
-  image(dog4, width * .09, height * .2)
 
 }
 
